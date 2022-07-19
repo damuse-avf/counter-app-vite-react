9990699c439c6c7bfd98c863dcbbb78b799843f2
Damaris Avila <damarisavilaf@gmail.com>
2022-07-19 00:32:02 -0400

    add pruebas

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,46 +1,42 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 
 
-export const CounterApp = ( {value} ) =>{
-  
+export const CounterApp = ({ value }) => {
+
     const [counter, setCounter] = useState(value)
     const handleAdd = () => {
-       setCounter( counter+1);
+        setCounter(counter + 1);
         //setCounter( (c) => c+1);
     }
+
     const handleSubstract = () => {
-        setCounter( counter-1);
-        
-     }
-     const handleReset = () => {
-        setCounter( value);
-      
-     }
+        setCounter(counter - 1);
+    }
+
+    const handleReset = () => {
+        setCounter(value);
+    }
+    
     return (
-          
         <>
             <h1> CounterApp</h1>
-           
-            <h2> { counter } </h2>
-            <button onClick={ handleAdd}>
+
+            <h2> {counter} </h2>
+            <button onClick={handleAdd}>
                 +1
             </button>
-            <button onClick={ handleSubstract}>
+            <button onClick={handleSubstract}>
                 -1
             </button>
-            <button onClick={ handleReset}>
-               reset
+            <button onClick={handleReset}>
+                reset
             </button>
-
         </>
-     
-   
-   
-    ) 
+    )
 }
 
 CounterApp.propTypes = {
-    value : PropTypes.number
+    value: PropTypes.number
 }
---
+++ tests/demo.test.js
@@ -0,0 +1,15 @@
+describe('pruebas en <DemoComponent />', () => { 
+    test("esta prueba no debe fallar message", () => {
+        // Inicializacion
+        // Estimulo
+        // Observar el comportamiento
+    
+        const message1 = 'Hola mundo';
+    
+        const message2 = message1.trim();
+    
+        expect(message1).toBe(message2);
+     
+    })
+ })
+
